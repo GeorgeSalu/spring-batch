075a7cda07783f491ae3fe1a1faf7319c4c59bb9
GeorgeSalu <george.salu10@gmail.com>
2022-12-15 11:56:32 -0300

    leitura de arquivo de largura fixa

--- ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/job/ArquivoLarguraFixaJobConfig.java
+++ ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/job/ArquivoLarguraFixaJobConfig.java
@@ -8,6 +8,9 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @EnableBatchProcessing
 @Configuration
