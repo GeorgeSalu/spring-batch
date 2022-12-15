92c5b117c239832cc0a7c2316a9fe5fa117769c1
GeorgeSalu <george.salu10@gmail.com>
2022-12-15 14:12:24 -0300

    restart na leitura de arquivo

--- ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/job/ArquivoLarguraFixaJobConfig.java
+++ ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/job/ArquivoLarguraFixaJobConfig.java
@@ -4,7 +4,6 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +19,6 @@ public class ArquivoLarguraFixaJobConfig {
 		return jobBuilderFactory
 				.get("arquivoLarguraFixaJob")
 				.start(leituraArquivoLarguraFixaStep)
-				//.incrementer(new RunIdIncrementer())
 				.build();
 	}
 }
