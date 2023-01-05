188d468b3821b7c5b692d77f842508578aab3707
GeorgeSalu <george.salu10@gmail.com>
2023-01-05 15:45:48 -0300

    leitura de arquivos delimitados

--- projeto_migracao_dados/MigracaoDadosJob/src/main/java/com/springbatch/migracaodados/job/MigracaoDadosConfig.java
+++ projeto_migracao_dados/MigracaoDadosJob/src/main/java/com/springbatch/migracaodados/job/MigracaoDadosConfig.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,7 +16,9 @@ public class MigracaoDadosConfig {
 	private JobBuilderFactory jobBuilderFactory;
 	
 	@Bean
-	public Job migracaoDadosJob(Step migrarPessoaStep,Step migrarDadosBancariosStep) {
+	public Job migracaoDadosJob(
+			@Qualifier("migrarPessoaStep") Step migrarPessoaStep,
+			@Qualifier("migrarDadosBancariosStep") Step migrarDadosBancariosStep) {
 		return jobBuilderFactory
 					.get("migracaoDadosJob")
 					.start(migrarPessoaStep)
