d347d23562ab6c1f8f1e01df98159403f711f983
GeorgeSalu <george.salu10@gmail.com>
2023-01-10 10:45:56 -0300

    configurações iniciais

---
+++ projeto_fatura_cartao_credito/FaturaCartaoCreditoJob/src/main/java/com/springbatch/faturacartaocredito/job/FaturaCartaoCreditojobConfig.java
@@ -0,0 +1,27 @@
+package com.springbatch.faturacartaocredito.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class FaturaCartaoCreditojobConfig {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Bean
+	public Job faturaCartaoCreditojob(Step faturaCartaoCreditoStep) {
+		return jobBuilderFactory
+				.get("faturaCartaoCreditojob")
+				.start(faturaCartaoCreditoStep)
+				.incrementer(new RunIdIncrementer())
+				.build();
+	}
+}
